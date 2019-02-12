4e5f88bbc9249a3a53802ff4989a31b5b5444c1b
jonolds <jonolds@gmail.com>
2019-02-11 21:50:29 -0600

    win

---
+++ src/WordCount.java
@@ -0,0 +1,76 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+public class WordCount {
+	public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable>{
+		private final static IntWritable one = new IntWritable(1);
+		private Text word = new Text();
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+			StringTokenizer itr = new StringTokenizer(value.toString());
+			while (itr.hasMoreTokens()) {
+				word.set(itr.nextToken());
+				context.write(word, one);
+			}
+		}
+	}
+	
+	public static class IntSumReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
+		private IntWritable result = new IntWritable();
+		public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+			int sum = 0;
+			for (IntWritable val : values) {
+				sum += val.get();
+			}
+			result.set(sum);
+			context.write(key, result);
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		Job job = initializeJob(args, "wordcount");
+		int ret0 = job.waitForCompletion(true) ? 0 : 1;
+		job = WordCountReverse.initializeJob(args, "wcRev");
+		int ret1 = job.waitForCompletion(true) ? 0 : 1;
+		
+		
+		System.out.println(ret0&ret1);
+		System.exit(ret0&ret1);
+	}
+	
+	public static Job initializeJob(String[] args, String jobname) throws IOException {
+		Configuration conf = new Configuration();
+		String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+		if (otherArgs.length != 2) {
+			System.err.println("Usage: wordcount <in> <out>");
+			System.exit(2);
+		}
+		String outfolder = "output_"+ jobname;
+		if(new File(outfolder).exists())
+			Files.walk(Paths.get(outfolder)).sorted(Comparator.reverseOrder()).forEach(x->x.toFile().delete());
+		
+		Job job = Job.getInstance(conf, jobname);
+		job.setJarByClass(WordCount.class);
+		job.setMapperClass(TokenizerMapper.class);
+		job.setCombinerClass(IntSumReducer.class);
+		job.setReducerClass(IntSumReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(outfolder));
+		return job;
+	}
+}
