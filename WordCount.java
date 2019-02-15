58a5eee8e845362c477b825f565fa7ae7b5878c4
jonolds <jonolds@gmail.com>
2019-02-14 21:33:56 -0600

    win

--- WordCount.java
+++ WordCount.java
@@ -1,6 +1,9 @@
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Comparator;
 import java.util.StringTokenizer;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -13,24 +16,25 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
 public class WordCount {
-	public static class TokenizerMapper extends Mapper<Object, Text, TextJon, IntWritable> {
+	public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable>{
 		private final static IntWritable one = new IntWritable(1);
-		private TextJon word = new TextJon();
+		private Text word = new Text();
 		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 			StringTokenizer itr = new StringTokenizer(value.toString());
 			while (itr.hasMoreTokens()) {
-				word.set(itr.nextToken().toLowerCase());
+				word.set(itr.nextToken());
 				context.write(word, one);
 			}
 		}
 	}
 	
-	public static class IntSumReducer extends Reducer<TextJon, IntWritable, TextJon, IntWritable> {
+	public static class IntSumReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
 		private IntWritable result = new IntWritable();
-		public void reduce(TextJon key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+		public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
 			int sum = 0;
-			for (IntWritable val : values)
+			for (IntWritable val : values) {
 				sum += val.get();
+			}
 			result.set(sum);
 			context.write(key, result);
 		}
@@ -42,7 +46,7 @@ public class WordCount {
 		job.setMapperClass(TokenizerMapper.class);
 		job.setCombinerClass(IntSumReducer.class);
 		job.setReducerClass(IntSumReducer.class);
-		job.setOutputKeyClass(TextJon.class);
+		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(IntWritable.class);
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
@@ -57,6 +61,8 @@ public class WordCount {
 			System.exit(2);
 		}
 		new Path(args[1]).getFileSystem(conf).delete(new Path(otherArgs[1]), true);
+
+		
 		Job job = Job.getInstance(conf, "word count");
 		return job;
 	}
